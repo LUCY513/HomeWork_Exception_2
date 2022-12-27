a32d7be356bf4fb1b96c9ab0dc89c3e2e21acb24
LUCY513 <97751920+LUCY513@users.noreply.github.com>
2022-12-27 21:42:20 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+// Реализуйте метод, который запрашивает у пользователя ввод дробного числа (типа float),
+// и возвращает введенное значение. Ввод текста вместо числа не должно приводить к падению приложения,
+// вместо этого, необходимо повторно запросить у пользователя ввод данных.
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("| Enter a number |");
+        Scanner inPut = new Scanner(System.in);
+        System.out.println("Your number --> " + enterFloatNumber(inPut));
+    }
+
+    public static float enterFloatNumber(Scanner futureNumber){
+        while(!futureNumber.hasNextFloat()){
+            System.out.println("Oops, ты опечатался дружок - посторонний символ в числе!");
+            futureNumber.next();
+        }
+        return futureNumber.nextFloat();
+    }
+}
